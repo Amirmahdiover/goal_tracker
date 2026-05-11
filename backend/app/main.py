6750d0a49b64bee809154b611adbca5f7d25bc38
Amirmahdi manafi <amirmahdi.manafi111@gmail.com>
2026-05-11 16:50:13 +0330

    for runflare chanigng main .py

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,6 +6,19 @@ from app.dependencies import header_scheme
 
 from fastapi.middleware.cors import CORSMiddleware
 
+# -----------------for run flare-------------------
+import os
+import sys
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BACKEND_DIR = os.path.join(BASE_DIR, "backend")
+
+sys.path.insert(0, BACKEND_DIR)
+
+from app.main import app
+
+
+
 # uvicorn app.main:app --reload
 # npm run dev
 Base.metadata.create_all(bind=engine)
