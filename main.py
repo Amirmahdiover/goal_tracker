0014f63241c8fdf129b80846a02cfcc85531dc6a
Amirmahdi manafi <149677062+Amirmahdiover@users.noreply.github.com>
2026-05-11 22:29:57 +0330

    main.py in root for runflare

---
+++ main.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BACKEND_DIR = os.path.join(BASE_DIR, "backend")
+
+sys.path.insert(0, BACKEND_DIR)
+
+from app.main import app
